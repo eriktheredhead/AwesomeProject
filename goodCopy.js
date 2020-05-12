98adba5f503d401072a9052b634e4758cb35c9b6
eriktheredhead <erikhough03@gmail.com>
2020-05-11 23:05:40 -0600

    new app todo practice

---
+++ goodCopy.js
@@ -0,0 +1,64 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, FlatList, TouchableOpacity } from 'react-native';
+
+export default function App() {
+  const [people, setPeople] = useState([
+    { name: 'shaun', id: '1'},
+    { name: 'yoshi', id: '2'},
+    { name: 'mario', id: '3'},
+    { name: 'luigi', id: '4'},
+    { name: 'peach', id: '5'},
+    { name: 'toad', id: '6'},
+    { name: 'bowser', id: '7'},
+  ]);
+  
+  const pressHandler = id => {
+    console.log(id);
+    setPeople((prevPeople) => {
+      return prevPeople.filter(person => person.id != id)
+    });
+  }
+
+  return (
+    <View style={styles.container}>
+
+      <FlatList
+        numColumns={2}
+        keyExtractor={(item) => item.id} 
+        data={people}
+        renderItem={({ item }) => (
+          <TouchableOpacity onPress={() => pressHandler(item.id)}>
+            <Text style={styles.item}>{item.name}</Text>
+          </TouchableOpacity>
+        )}
+      />
+      
+    {/* <ScrollView>
+    { people.map(item => (
+        <View id={item.id}>
+          <Text style={styles.item}>{item.name}</Text>
+        </View>
+      ))}
+    </ScrollView> */}
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    paddingTop: 40,
+    paddingHorizontal: 20
+    // alignItems: 'stretch',
+    // justifyContent: 'space-between',
+  },
+  item: {
+    marginTop: 24,
+    padding: 30,
+    backgroundColor: '#ff9933',
+    fontSize: 24,
+    marginHorizontal: 10,
+  }
+});
